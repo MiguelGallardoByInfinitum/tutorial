199f6683f2bdcc40a7f76f0ac8829a770a5de05d
MiguelGallardoByInfinitum <159464688+MiguelGallardoByInfinitum@users.noreply.github.com>
2024-02-29 17:55:09 +0100

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://unique-kulfi-3dd027.netlify.app/'
+});
