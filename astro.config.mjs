84ae6f91a7435deea7067c1c616ec65be3090a3c
MiguelGallardoByInfinitum <159464688+MiguelGallardoByInfinitum@users.noreply.github.com>
2024-03-01 14:46:41 +0100

    end tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://unique-kulfi-3dd027.netlify.app'
+  site: 'https://unique-kulfi-3dd027.netlify.app',
+  integrations: [preact()]
 });
