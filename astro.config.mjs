fc87a4cbee2c571de5c6ca133320ff1718106a43
MiguelGallardoByInfinitum <159464688+MiguelGallardoByInfinitum@users.noreply.github.com>
2024-03-01 14:03:42 +0100

    a ver si funsiona

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://unique-kulfi-3dd027.netlify.app/'
+  site: 'http://localhost:4321/'
 });
